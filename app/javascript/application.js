559d71d8a10bceee614acc65a299dfedec3d19c6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-03-30 21:32:27 +0200

    load js on certain pages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+//= stub 'search_posts'
+//= stub 'search_users'
+let search_input;
+let whatToSearch;
