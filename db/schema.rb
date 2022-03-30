ffb3a4cbbeab4fdba87c5b337c9203a3b8e01e4b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-03-30 20:16:15 +0200

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_26_205008) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_181529) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_205008) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "bio"
+    t.string "picture_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
