b560dea7f9e706696e12c92250d9e68cae69e1f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-11 23:34:51 +0100

    fix post policy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,8 +1,28 @@
 class PostPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+
+    def index?
+      true
+    end
+
+    def show?
+      true
+    end
+
+    def create?
+      @user.present?
+    end
+
+    def update?
+      @user.present? && (@record.user == @user)
+    end
+
+    def destroy?
+      @user.present? && (@record.user == @user)
+    end
   end
 end
