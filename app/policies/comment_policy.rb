642f957cc26e140f8b83517e4c1048c8e70a88c4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-11 23:37:25 +0100

    update comment policy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,8 +1,16 @@
 class CommentPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+
+    def create?
+      @user.present?
+    end
+
+    def destroy?
+      @user.present? && (@record.user == @user)
+    end
   end
 end
