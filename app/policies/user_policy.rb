b153239f75257789f690296e4fb7aedae15a1f9d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-04-11 23:30:57 +0100

    fix policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,28 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    @user.present?
+  end
+
+  def update?
+    @user.present? && @user == @record
+  end
+
+  def destroy?
+    @user.present? && @user == @record
   end
 end
